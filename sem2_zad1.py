ea7b5f4558d96e072059d89fe3d29e709906d994
Elena <9513495670@mail.ru>
2022-10-17 14:14:16 +0300

    новое решение

--- sem2_zad1.py
+++ sem2_zad1.py
@@ -3,11 +3,12 @@
 #Пример:
 #- 6782 -> 23
 #- 0,56 -> 11
-
+import functools
 num = input('Введите вещественное число: ')
-count=0
-for i in range(0,len(num)):
-    if num[i]!=',':
-        count+=int(num[i])
+
+def chek(n):
+    if n not in (',','.'):
+        return n
+count = functools.reduce(lambda x,y: x+y,map(int,(filter(chek, num))))
 
 print(f'{num} -> {count}')
